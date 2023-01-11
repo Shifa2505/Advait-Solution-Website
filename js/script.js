dd3859c1b72c113b88164483d3533744d6c77b5a
Shifa25 <shifashaikh2505@gmail.com>
2023-01-11 20:04:13 +0530

    commit eight

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".testimonial-content", {
     slidesPerView: 1,
     spaceBetween: 60,
     loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -11,3 +14,5 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // var swiper = new Swiper(".mySwiper", {
